cb9d26e89c9393bc6befb75da84c1e4a60bd7bc3
Berlin Sky <pedroyoung8989@gmail.com>
2016-09-08 14:42:32 -0700

    Remove obsolete files

--- app/scripts/application/application-base.js
+++ app/scripts/application/application-base.js
@@ -21,16 +21,7 @@ export class ApplicationBase {
       this.defaultRoute = null;
     }
 
-  //  enableRoute(routeId) {
-  //   let component = this.navBar.component.find('.page-content');
-  //   component.empty();
-
-  //   console.log('component in app >', component);
-  //   this.routes[routeId].appendToComponent(component);
-  // }
-    
   createRoute(routeId, pageComponent, isDefault = false) {
-    // this.navBar.addLink(routeId, '');
     this.routes[routeId] = pageComponent;
 
     if (isDefault) {
@@ -39,16 +30,12 @@ export class ApplicationBase {
   }
     
   render(component) {
-    // this.navBar.appendToComponent(component);
     this.layout.appendToComponent(component);
     this._appendNavigation();
     this._appendSearchPanel();                                
     this._appendFooter();
 
     this._attachSearchEvent()
-    // if (this.defaultRoute) {
-    //   this.enableRoute(this.defaultRoute);
-    // }
   }
 
    _appendNavigation() {
@@ -61,21 +48,6 @@ export class ApplicationBase {
     const searchPanel = new SearchPanel();
     const mainContentContainer = this.layout.component.find('.js-mainContentContainer');
     searchPanel.appendToComponent(mainContentContainer);
-
-    // const dataService = new BookDataService();
-    // dataService.populateData(books);
-    // const bookList = dataService.getBookListSortedByTitle();
-    // const bookList = dataService.getBookListSortedByIsbn();
-
-    // const divider = [2, 3, 3, 3, 3, 2];
-    // const tableHeader = "isbn author title publisher pubdate price".split(' ');
-    // const gridTable = new GridTable(divider, tableHeader, bookList);
-    // gridTable.setStyles("background-color: #999; color: #333; padding: 5px 10px;");
-    // gridTable.appendToComponent(mainContentContainer);
-
-    // const bookSearchResult = new BookSearchResult();
-    // bookSearchResult.setStyles("background-color: #999; color: #333; padding: 5px 10px;");
-    // bookSearchResult.appendToComponent(mainContentContainer);
   } 
 
   _appendFooter() {
@@ -89,7 +61,6 @@ export class ApplicationBase {
     const searchInput = this.layout.component.find('.js-searchBar__inputText');
     const mainContentContainer = this.layout.component.find('.js-mainContentContainer');
 
-    // searchButton.click(function() {
     searchInput.keyup(function() {
       let searchResultContainer = mainContentContainer.find('.js-gridTable');
       searchResultContainer.remove();
@@ -109,8 +80,6 @@ export class ApplicationBase {
       else {
         bookList = dataService.searchBooksByTitle(searchText);
       }
-      // const bookList = dataService.getBookListSortedByIsbn();
-
 
       const divider = [2, 3, 3, 3, 3, 2];
       const tableHeader = "isbn author title publisher pubdate price".split(' ');
